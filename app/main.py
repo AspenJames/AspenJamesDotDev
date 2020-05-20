68048431b5d721a3cd887bea5e497a05d6c0a375
Aspen James <aspen@pop-os.localdomain>
2020-05-19 17:35:13 -0700

    Add /recompile, add header
    Adds a header component to the default layout.
    Adds a /recompile route to recompile scss during dev

--- app/main.py
+++ app/main.py
@@ -7,17 +7,13 @@ import sass
 
 from starlette.applications import Starlette
 from starlette.endpoints import HTTPEndpoint
+from starlette.responses import JSONResponse
 from starlette.routing import Mount
 from starlette.routing import Route
 from starlette.staticfiles import StaticFiles
 from starlette.templating import Jinja2Templates
 
 app_dir = os.path.dirname(os.path.realpath(__file__))
-
-# Compile SCSS at boot
-sass.compile(dirname=('{}/static/scss'.format(app_dir),
-                      '{}/static/css'.format(app_dir)),
-             output_style='compressed')
 
 templates = Jinja2Templates(directory='app/templates')
 
@@ -29,9 +25,25 @@ class Home(HTTPEndpoint):
         return templates.TemplateResponse('index.html', {'request': request})
 
 
+async def recompile(request):
+    try:
+        compile_scss()
+        return JSONResponse({'200': 'OK'})
+    except Exception as e:
+        return JSONResponse({'error': e})
+
+
 routes = [
     Route("/", Home),
+    Route("/recompile", recompile),
     Mount('/static', app=StaticFiles(directory='app/static'), name='static'),
 ]
 
+def compile_scss():
+    sass.compile(dirname=('{}/static/scss'.format(app_dir),
+                        '{}/static/css'.format(app_dir)),
+                output_style='compressed')
+
+# Compile SCSS at boot
+compile_scss()
 app = Starlette(debug=True, routes=routes)
